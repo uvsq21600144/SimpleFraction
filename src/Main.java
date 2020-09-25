301e728144c454330cbfff40d8f04ac290e933f2
Mélanie Bonnet <melanie.bonnet@ens.uvsq.fr>
2020-09-25 10:12:44 +0200

    used .gitignore

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("hello world");
         
+        
         Fraction fraction = new Fraction (1,2);
         
         fraction.toString();
+        
+        System.out.println("hello world");
         
         
     }
