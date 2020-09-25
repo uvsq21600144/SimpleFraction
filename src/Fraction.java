7bda944cb819ec96fe9eb8c289d12fc22784fd96
Mélanie Bonnet <melanie.bonnet@ens.uvsq.fr>
2020-09-25 09:19:10 +0200

    java fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,22 @@
 
 public class Fraction {
+	
+	private int x;
+	private int y;
+	
+	
+	public Fraction (int x, int y) {
+			this.x = x;
+			this.y = y;
+		
+	}
+	
+	public String toString() {
+			
+		System.out.println("fraction  : " + this.x + "/" + this.y);
+		
+		return "fraction  : " + this.x + "/" + this.y;
+	}
+	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("hello world");//appel de la methode static de la classe calcul
+        System.out.println("hello world");
+        
+        Fraction fraction = new Fraction (1,2);
+        
+        fraction.toString();
+        
+        
     }
 }
